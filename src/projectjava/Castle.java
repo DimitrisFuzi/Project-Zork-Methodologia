5fa05a76545f176d3ccf970f1025ee461cddeb33
geormarga <geormarga@users.noreply.github.com>
2015-03-19 12:58:10 +0200

    Update Castle.java

--- src/projectjava/Castle.java
+++ src/projectjava/Castle.java
@@ -6,58 +6,22 @@
 package textbasedadventure;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  *
  * @author Αέναος
  */
-public class Castle implements IZorkRoom {
+public class Castle extends ZorkRoom {
 
-    private String description;
+    private String description = "You are in the castle.Vast pillars are blocking your way";
     private HashMap exits;        // stores exits of this room.
 
     @Override
-    public void setExits(IZorkRoom north, IZorkRoom east, IZorkRoom south, IZorkRoom west) {
-        if (north != null) {
-            exits.put("north", north);
-        }
-        if (east != null) {
-            exits.put("east", east);
-        }
-        if (south != null) {
-            exits.put("south", south);
-        }
-        if (west != null) {
-            exits.put("west", west);
-        }
+    public void setExits(HashMap par) {
 
-    }
-
-    @Override
-    public HashMap getExits() {
-        return exits;
-    }
-
-    @Override
-    public void setDescription() {
-        description = "You are in the castle.Vast pillars are blocking your way";
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public String exitString() {
-        String returnString = "Exits:";
-        Set keys = exits.keySet();
-        for (Iterator iter = keys.iterator(); iter.hasNext();) {
-            returnString += " " + iter.next();
+        if (par.containsKey("Forest")) {
+            exits.put("go west", par.get("Forest"));
         }
-        return returnString;
     }
 
 }
