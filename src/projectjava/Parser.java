51d7fd521552bef34fd8b1c13ad7df70330b2052
geormarga <geormarga@users.noreply.github.com>
2015-03-19 13:03:04 +0200

    Update Parser.java

--- src/projectjava/Parser.java
+++ src/projectjava/Parser.java
@@ -3,24 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projectjava;
-
+package textbasedadventure;
 
 /**
  *
  * @author Αέναος
  */
 public class Parser {
--
--    public void checkCommand(String command, IZorkRoom currentRoom) {
--
--        if (currentRoom.getExits().containsKey(command)) {
--
--            RoomTraveler Travel = new RoomTraveler();
--            Travel.switchRooms(command, currentRoom);
--
--        } else {
--            System.out.println("I didn't recognise the command.");
--        }
--    }
--}
+
+    public void checkCommand(String command, ZorkRoom currentRoom) {
+
+        if (currentRoom.getExits().containsKey(command)) {
+
+            RoomTraveler Travel = new RoomTraveler();
+            Travel.switchRooms(command, currentRoom);
+
+        } else {
+            System.out.println("I didn't recognise the command.");
+        }
+    }
+}
